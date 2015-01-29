c7a6ed5857ab63f7669a00e3f09ebbd8a4819f77
jebarnet <jebarnet@gmail.com>
2015-01-29 20:13:20 +0000

    Modifying production file to figure out sign up issue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  #config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => "testenterpriseape.herokuapp.com" }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
 end
